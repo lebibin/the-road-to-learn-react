685f42504f9fb6ba2fde2e1b670d670484ea271d
Kevin Candelaria <kevdashdev@gmail.com>
2017-02-14 22:04:12 +0800

    Welcome to react

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    var helloWorld = "Welcome to React";
+    return(
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{helloWorld}</h2>
       </div>
     );
   }
